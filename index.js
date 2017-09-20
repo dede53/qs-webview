5cdb8d6aafe4fd7053e463f5dfd4ca519a7e7243
dede53 <hallomarcel.hm@gmail.com>
2017-09-20 03:32:35 +0200

    commit

--- index.js
+++ index.js
@@ -1,9 +1,21 @@
 var express					=	require('express.oi');
-var app						=	express().http().io();
+//var app						=	express().http().io();
 var adapter					=	require('../../adapter-lib.js');
 var webview					=	new adapter("webview");
 var status					=	{};
 var timeout					=	"";
+var fs						=	require('fs');
+
+if(webview.settings.useHTTPS){
+	var options = {
+		key: fs.readFileSync('./key.pem'),
+		cert: fs.readFileSync('./cert.pem')
+	}
+	var app						=	express().https(options).io();
+}else{
+	var app						=	express().http().io();
+}
+
 app.use(express.static('/home/pi/QuickSwitch2.0/SwitchServer/adapter/webview/dist'));		// provides static htmls
 
 app.get('/pc', function(req, res) {
