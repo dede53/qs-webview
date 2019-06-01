ec290d3cd0f9a92ddf63c66b5c8e8616736965a1
dede53 <hallomarcel.hm@gmail.com>
2019-06-01 10:54:30 +0200

    moved all config for the chart to app.service.global.ts

--- src/app/app.service.global.ts
+++ src/app/app.service.global.ts
@@ -25,88 +25,105 @@ export class GlobalObjectsService {
         favoritVariables:{},
         hideChart: true,
         chartOptions: {
-                chart: {
-                    backgroundColor: 'transparent',
-                    zoomType:"x"
-                },
-                plotOptions: {
-                    series: {
-                        marker:{
-                            enabled: false
+            chart: {
+                backgroundColor: '#ffffff',
+                zoomType: "x",
+                resetZoomButton: {
+                    theme: {
+                        display: "none"
+                    }
+                }
+            },
+            plotOptions: {
+                series: {
+                    marker: {
+                        enabled: false
+                    },
+                    states: {
+                        hover: {
+                            enabled: true,
+                            lineWidthPlus: 1.5
                         },
-                        animation: false
+                        inactive: {
+                            opacity: 1
+                        }
                     }
-                },
-                xAxis: [{
-                    type: 'datetime',
-                    labels:{
-                        rotation: 0,
-                        style: {
-                            "color": '#80a3ca',
-                            "fontSize": "16px"
-                        }
-                    },
-                    events: {
-                        afterSetExtremes: function(e){
-                            console.log(e);
-                        }
-                    },
-                    dateTimeLabelFormats: {
-                        second: '%d.%m<br/>%H:%M:%S',
-                        minute: '%d.%m<br/>%H:%M',
-                        hour: '%d.%m<br/>%H:%M',
-                        day: '%d.%m<br/>%H:%M',
-                        week: '%d.%m.%Y',
-                        month: '%m.%Y',
-                        year: '%Y'
-                    }
-                }],
-                series: [
-                    {
-                        data: [1, 2, 7]
-                      }],
-                yAxis: [{
-                    allowDecimals: true,
-                    title: {
-                        text: 'Temperatur',
-                        style: {
-                            "color": '#80a3ca',
-                            "fontSize": "16px"
-                        }
-                    },
-                    labels: {
-                        format: '{value}',
-                        style: {
-                            "color": '#80a3ca',
-                            "fontSize": "16px"
-                        }
-                    },
-                    plotLines: [/*{
-                        value: 5,
-                        color: '#444488',
-                        dashStyle: 'shortdash',
-                        width: 2,
-                        label: {
-                            text: '5°C'
-                        }
-                    }*/]
-                }],
-
-                legend: {
-                    enabled: true,
-                    itemStyle: {
+                }
+            },
+            xAxis: [{
+                type: 'datetime',
+                // lineColor:'#4d4d4d',
+                // tickColor:'#4d4d4d',
+                // minorGridLineWidth: 0,
+                // minorTickInterval: 3600 * 1000, // Hourly
+                // minorTickInterval: 'auto', // Hourly
+                // minorTickColor: '#4d4d4d',
+                // minorTickWidth: 1,
+                // minorTickLength: 7,
+                labels: {
+                    rotation: 0,
+                    style: {
+                        "color": '#4d4d4d',
                         "fontSize": "16px"
                     }
                 },
-                title: {
-                    text: ''
+                events: {
+                    afterSetExtremes: function (e) {
+                        console.log(Math.round(e.min));
+                        console.log(Math.round(e.max));
+                    }
                 },
-                credits: {
-                    enabled: false
+                dateTimeLabelFormats: {
+                    second: '%d.%m<br/>%H:%M:%S',
+                    minute: '%d.%m<br/>%H:%M',
+                    hour: '%d.%m<br/>%H:%M',
+                    day: '%d.%m<br/>%H:%M',
+                    week: '%d.%m.%Y',
+                    month: '%m.%Y',
+                    year: '%Y'
+                }
+            }],
+            series: [],
+            yAxis: [{
+                allowDecimals: true,
+                opposite: true,
+                gridLineColor: '#bfbfbf',
+                lineColor: '#4d4d4d',
+                title: {
+                    text: 'Temperatur',
+                    style: {
+                        "color": '#4d4d4d',
+                        "fontSize": "16px"
+                    }
+                },
+                labels: {
+                    format: '{value}',
+                    style: {
+                        "color": '#4d4d4d',
+                        "fontSize": "16px"
+                    }
+                }
+            }],
+            tooltip:{
+                dateTimeLabelFormats:{
+                    second: '%A, %e %b, %H:%M:%S'
                 }
             },
+            legend: {
+                enabled: true,
+                itemStyle: {
+                    "fontSize": "16px"
+                }
+            },
+            title: {
+                text: ''
+            },
+            credits: {
+                enabled: false
+            }
+        },
         chart: void{},
-        updateFlag: true,
+        updateFlag: true
     };
     activeUser = {
         name:"",
